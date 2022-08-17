d92732af152df4db5436d62a4afabbde300dc37c
Manuel Pineda <manuel.pineda@invictusbpo.com>
2022-08-17 13:18:18 -0600

    finished substring

--- minimum-window-substring.js
+++ minimum-window-substring.js
@@ -1,9 +1,55 @@
 let minWindow = function(s, t) {
-  let left = 0
-  let freq = new Map()
+  let map = new Map()
 
   for(let i = 0; i < t.length; i++) {
-
+    let char = t[i]
+    map.set(char, (map.get(char) || 0) + 1)
   }
 
+  let i = 0;
+  let j = 0;
+  let count = map.size
+
+  let left = 0
+  let right = s.length - 1
+  let minLength = s.length
+
+  let found = false
+
+  while (j < s.length) {
+   let endChar = s.charAt(j++)
+
+    if (map.has(endChar)) {
+      map.set(endChar, map.get(endChar) - 1)
+
+      if (map.get(endChar) === 0) {
+        count--
+      }
+    }
+
+    if (count > 0) continue;
+
+    while (count === 0) {
+      let startChar = s.charAt(i++)
+
+      if (map.has(startChar)) {
+        map.set(startChar, map.get(startChar) + 1)
+
+        if (map.get(startChar) > 0) {
+          count++
+        }
+      }
+    }
+
+    if ((j - i) < minLength) {
+      left = i
+      right = j
+      minLength = j - i
+      found = true
+    }
+  }
+
+  return !found ? "" : s.substring(left -1, right)
 };
+
+console.log(minWindow("ADOBECODEBANC", "ABC"))
